d9dd6c7c12e3fef37fe62edbeaa2ecc6839db39a
Wésley Guimarães <wesley.coder@gmail.com>
2024-04-24 08:34:42 -0300

    style: oh my eyes

--- apps/tmdb/astro.config.mjs
+++ apps/tmdb/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config'
 import deno from '@astrojs/deno'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   output: 'server',
   adapter: deno(),
+  integrations: [tailwind()],
   image: {
     service: passthroughImageService(),
   },
